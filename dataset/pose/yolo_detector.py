fd12285e79bd96a4cdee53d6daec4079488f89e1
Cas van Rijbroek <31927687+Casvanrijbroek@users.noreply.github.com>
2023-04-26 12:01:12 +0200

    added error logging on error during pose prediction

--- dataset/pose/yolo_detector.py
+++ dataset/pose/yolo_detector.py
@@ -10,7 +10,7 @@ from utils.config import Config
 import pandas as pd
 
 
-def process_dataset(output_dir):
+def process_dataset(output_dir, log_file='log.txt'):
     config = Config()
     overview_path = config.content['overview']
     df_overview = pd.read_csv(overview_path)
@@ -23,11 +23,20 @@ def process_dataset(output_dir):
     if not os.path.exists(output_dir / 'keypoints'):
         os.mkdir(output_dir / 'keypoints')
 
+    log_handle = open(log_file, 'w')
+
     for _, row in df_overview.iterrows():
         unique_id = f"{row['session_id']}_{row['speaker_id']}"
-        process_video(unique_id, row['media_path'], output_dir)
 
-        break
+        # noinspection PyBroadException
+        try:
+            process_video(unique_id, row['media_path'], output_dir)
+            log_handle.write(f'Successfully processed {unique_id}')
+        except Exception as err:
+            log_handle.write(f'Error processing {unique_id}\n')
+            log_handle.write(str(err) + '\n')
+
+    log_handle.close()
 
 
 def process_video(unique_id, video_path, output_dir):
@@ -48,17 +57,10 @@ def process_video(unique_id, video_path, output_dir):
     boxes_list = []
     keypoints_list = []
 
-    prev_nose_x = 0
-    prev_nose_y = 0
-
     while capture.isOpened():
         ret, frame = capture.read()
 
         if ret:
-            # TODO: remove
-            if idx < 510:
-                idx += 1
-                continue
             frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             results = model(frame_rgb)
 
@@ -69,43 +71,14 @@ def process_video(unique_id, video_path, output_dir):
             if n_results == 0:
                 boxes_list.append(np.zeros((1, 6)))
                 keypoints_list.append(np.zeros((1, 17, 3)))
-
-                # output_video.write(frame)
-                # TODO: remove
-                output_frame = results[0].plot()
-                output_frame = cv2.cvtColor(output_frame, cv2.COLOR_RGB2BGR)
-                cv2.imshow('no detections', output_frame)
-                key = cv2.waitKey(0)
-                while key not in [ord('q'), ord('k')]:
-                    key = cv2.waitKey(0)
             else:
-                if n_results == 1:
-                    result_id = 0
-                else:
-                    cur_nose_x = keypoints[:, 0, 0]
-                    cur_nose_y = keypoints[:, 0, 1]
-                    diff_x = cur_nose_x - prev_nose_x
-                    diff_y = cur_nose_y - prev_nose_y
-                    diff = np.abs(diff_x + diff_y)
-                    result_id = np.argmin(diff)
-
-                    output_frame = results[0].plot()
-                    output_frame = cv2.cvtColor(output_frame, cv2.COLOR_RGB2BGR)
-                    cv2.imshow('2 detections', output_frame)
-                    # TODO: remove
-                    key = cv2.waitKey(0)
-                    while key not in [ord('q'), ord('k')]:
-                        key = cv2.waitKey(0)
-
-                boxes_list.append(boxes[result_id, :])
-                keypoints_list.append(keypoints[result_id, :, :])
-
-                prev_nose_x = keypoints[result_id, 0, 0]
-                prev_nose_y = keypoints[result_id, 0, 1]
+                boxes_list.append(boxes)
+                keypoints_list.append(keypoints)
 
             output_frame = results[0].plot()
             output_frame = cv2.cvtColor(output_frame, cv2.COLOR_RGB2BGR)
             output_video.write(output_frame)
+
             idx += 1
         else:
             break
@@ -113,8 +86,8 @@ def process_video(unique_id, video_path, output_dir):
     capture.release()
     output_video.release()
 
-    boxes = np.stack(boxes_list)
-    keypoints = np.stack(keypoints_list)
+    boxes = stack_with_padding(boxes_list)
+    keypoints = stack_with_padding(keypoints_list)
 
     np.save(output_dir / 'boxes' / f'{unique_id}.npy', boxes)
     np.save(output_dir / 'keypoints' / f'{unique_id}.npy', keypoints)
@@ -123,8 +96,21 @@ def process_video(unique_id, video_path, output_dir):
     assert n_frames == keypoints.shape[0]
 
 
+def stack_with_padding(array_list, variable_dim=0):
+    padded_list = []
+    shape = list(array_list[0].shape)
+
+    sizes = [array.shape[variable_dim] for array in array_list]
+    max_size = max(sizes)
+
+    for i in range(len(array_list)):
+        padding_shape = shape
+        padding_shape[0] = max_size - array_list[i].shape[0]
+        padded_list.append(np.concatenate([array_list[i], np.zeros(padding_shape)]))
+
+    return np.stack(padded_list)
+
+
 if __name__ == '__main__':
-    # process_dataset(r"E:\Results\pose_detection")
-    # main(r"E:\CorpusNGT\CNGT_720p\CNGT2103_S084_b_720.mp4")
-    # main(r"E:\CorpusNGT\CNGT_720p\CNGT0002_S004_b_720.mp4")
-    process_video('CNGT1590_S068', r"E:\CorpusNGT\CNGT_720p\CNGT1590_S068_b_720.mp4", Path("E:\Results\pose_detection"))
+    process_dataset(r"E:\Results\pose_detection")
+    # process_video('CNGT1590_S068', r"E:\CorpusNGT\CNGT_720p\CNGT1590_S068_b_720.mp4", Path("E:\Results\pose_detection"))
