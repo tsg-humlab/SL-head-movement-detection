9521c5839ed0c75e81b53b25e1a3b080bafa9c6e
Cas van Rijbroek <31927687+Casvanrijbroek@users.noreply.github.com>
2023-05-04 14:36:12 +0200

    find and draw subject

--- pose/review_conflicts.py
+++ pose/review_conflicts.py
@@ -35,7 +35,12 @@ def review_case(keypoints_file, boxes_file, media_file):
 
     for i, frame in enumerate(keypoints):
         if not np.array_equal(frame[1], empty_prediction):
-            key = show_frame(capture, i, media_file.stem)
+            target_signer = find_subject(keypoints[i], boxes[i])
+
+            key = show_frame(capture,
+                             i,
+                             target_bbox=boxes[i, target_signer, :4],
+                             title=media_file.stem)
 
             if key == ord('q'):
                 break
@@ -45,9 +50,18 @@ def review_case(keypoints_file, boxes_file, media_file):
                 for j in range(len(keypoints[i])):
                     if not np.array_equal(keypoints[i, j], empty_prediction):
                         print(f'Person conf: {np.round(boxes[i, j, 4], 2)}')
-                        print(f'nose       : x = {np.round(keypoints[i, j, 0, 0])} | y = {np.round(keypoints[i, j, 0, 1])} | conf = {np.round(keypoints[i, j, 0, 2], 2)}')
-                        print(f'eye (left) : x = {np.round(keypoints[i, j, 0, 0])} | y = {np.round(keypoints[i, j, 0, 1])} | conf = {np.round(keypoints[i, j, 0, 2], 2)}')
-                        print(f'eye (right): x = {np.round(keypoints[i, j, 0, 0])} | y = {np.round(keypoints[i, j, 0, 1])} | conf = {np.round(keypoints[i, j, 0, 2], 2)}')
+                        print(f'nose       : '
+                              f'x = {np.round(keypoints[i, j, 0, 0])} | '
+                              f'y = {np.round(keypoints[i, j, 0, 1])} | '
+                              f'conf = {np.round(keypoints[i, j, 0, 2], 2)}')
+                        print(f'eye (left) : '
+                              f'x = {np.round(keypoints[i, j, 0, 0])} | '
+                              f'y = {np.round(keypoints[i, j, 0, 1])} | '
+                              f'conf = {np.round(keypoints[i, j, 0, 2], 2)}')
+                        print(f'eye (right): '
+                              f'x = {np.round(keypoints[i, j, 0, 0])} | '
+                              f'y = {np.round(keypoints[i, j, 0, 1])} | '
+                              f'conf = {np.round(keypoints[i, j, 0, 2], 2)}')
                         print('-' * 50)
                 print('#' * 50)
                 cv2.waitKey()
@@ -56,7 +70,7 @@ def review_case(keypoints_file, boxes_file, media_file):
     cv2.destroyAllWindows()
 
 
-def show_frame(capture, frame_i, title='CNGT frame'):
+def show_frame(capture, frame_i, target_bbox=None, title='CNGT frame'):
     capture.set(cv2.CAP_PROP_POS_FRAMES, frame_i)
     ret, frame = capture.read()
 
@@ -66,12 +80,40 @@ def show_frame(capture, frame_i, title='CNGT frame'):
 
         raise RuntimeError(f"Couldn't load frame {frame_i} from {title}")
 
+    if target_bbox is not None:
+        draw_bbox(frame, target_bbox)
+
     cv2.imshow(title, frame)
     return cv2.waitKey()
 
 
-def get_frame_ratio(frame, duration, fps):
-    return frame / (duration * fps)
+def draw_bbox(frame, bbox):
+    x1 = int(bbox[0])
+    y1 = int(bbox[1])
+    x2 = int(bbox[2])
+    y2 = int(bbox[3])
+
+    sub_img = frame[y1:y2, x1:x2]
+    green_rect = np.zeros(sub_img.shape, dtype=np.uint8)
+    green_rect[:, :, 1] = 255
+    res = cv2.addWeighted(sub_img, 0.5, green_rect, 0.5, 1.0)
+    frame[y1:y2, x1:x2] = res
+
+
+def find_subject(keypoints, boxes):
+    """Find the subject of the video using a simple weighted average of the confidence values.
+
+    The eyes and nose of the subject are averaged together
+
+    :param keypoints:
+    :param boxes:
+    :return:
+    """
+    face_conf = np.average(keypoints[:, :3, 2], axis=1)
+    bbox_conf = boxes[:, 4]
+    weighted_conf = (face_conf + bbox_conf) / 2
+
+    return np.argmax(weighted_conf)
 
 
 if __name__ == '__main__':
